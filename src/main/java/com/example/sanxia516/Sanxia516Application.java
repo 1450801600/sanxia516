476ccef56f5955527a362e235e0555f7de415694
1450801600 <1450801600@qq.com>
2022-08-11 16:37:41 +0800

    sanxia516项目初始化

---
+++ src/main/java/com/example/sanxia516/Sanxia516Application.java
@@ -0,0 +1,13 @@
+package com.example.sanxia516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sanxia516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sanxia516Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sanxia516/Sanxia516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sanxia516;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sanxia516ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
